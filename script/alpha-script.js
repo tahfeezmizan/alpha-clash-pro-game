aa225c72a1f129a262745a26f39b446d13e25d54
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-02-14 01:05:34 +0600

    add functionality

---
+++ script/alpha-script.js
@@ -0,0 +1,25 @@
+// function hideElementById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.add('hidden')
+// }
+
+
+// function play() {
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden')
+//     // console.log(homeSection);
+
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden')
+//     // console.log(playgroundSection.classList)
+// }
+
+
+function continueGame({
+    
+})
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+}
---
+++ script/utlity.js
@@ -0,0 +1,14 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+
+
+function getArandomAlphabet(){
+    const alphabets = [ ]
+}
