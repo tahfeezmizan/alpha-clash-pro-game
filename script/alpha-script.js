0330807b44357240a2d8960dbd64015252f32d16
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-02-14 12:27:26 +0600

    add characters bg

--- script/alpha-script.js
+++ script/alpha-script.js
@@ -1,25 +1,25 @@
-// function hideElementById(elementId){
-//     const element = document.getElementById(elementId);
-//     element.classList.add('hidden')
-// }
 
-
-// function play() {
-//     const homeSection = document.getElementById('home-screen');
-//     homeSection.classList.add('hidden')
-//     // console.log(homeSection);
-
-//     const playgroundSection = document.getElementById('play-ground');
-//     playgroundSection.classList.remove('hidden')
-//     // console.log(playgroundSection.classList)
-// }
-
-
-function continueGame({
-    
-})
-
-function play(){
+function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
+    continueGame()
 }
+
+
+function continueGame() {
+    // get generate random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('random alphabet', alphabet)
+
+    // set randomly generated alphabet to the screen
+    let currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerHTML = alphabet;
+
+    // set background color 
+    setBackgroundColorById(alphabet);
+
+}
+
+
+
+
--- script/utlity.js
+++ script/utlity.js
@@ -1,14 +1,34 @@
-function hideElementById(elementId){
+function hideElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
 }
 
-function showElementById(elementId){
+function showElementById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 }
 
-
-function getArandomAlphabet(){
-    const alphabets = [ ]
+// characters bg color add
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add("bg-yellow-600");
 }
+
+function getARandomAlphabet() {
+    // create an alphabet array
+    const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'f', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+
+    // create random index generator between 0 - 25;
+    const randomNumber = Math.random() * 25;
+    const randomIndex = Math.round(randomNumber);
+
+    const alphabet = alphabets[randomIndex];
+    return alphabet;
+}
+// getARandomAlphabet()
+
+
+
+
+
