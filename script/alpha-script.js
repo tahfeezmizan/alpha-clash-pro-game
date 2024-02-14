329b5e1f460085ed4e455ec10c4c5f727c060392
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-02-14 21:49:02 +0600

    live score increase and life decrease program

--- script/alpha-script.js
+++ script/alpha-script.js
@@ -13,11 +13,33 @@ function handleKeyboardButtonPress(event){
     //check match or not
     if(playerPressedKey === expectedAlphabet){
         console.log('you have pressed correctly', expectedAlphabet);
+
+        // update score 
+        // 1. get current score 
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText)
+        console.log(currentScore);
+
+        // 2. increase the score 
+        const newScore = currentScore + 1;
+
+        // 3. show the update score 
+        currentScoreElement.innerText = newScore;
+
+        // start new round
         removeBackgroundColorById(expectedAlphabet)
         continueGame()
     }
     else{
-        console.log('You lost a life')
+        console.log('You lost a life');
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const newLife = currentLife - 1;
+
+        currentLifeElement.innerText = newLife
     }
 }
 
